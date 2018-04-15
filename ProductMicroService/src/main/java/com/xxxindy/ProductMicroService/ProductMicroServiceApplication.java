e947f2d6f16f267ee03d398ca5b98d5cb2c367f0
zhangxindi <zhangxindi@qbb6.com>
2018-04-15 11:08:50 +0800

    ProductMicroService-init
    ProductMicroService-init

---
+++ ProductMicroService/src/main/java/com/xxxindy/ProductMicroService/ProductMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.xxxindy.ProductMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroServiceApplication.class, args);
+	}
+}
---
+++ ProductMicroService/src/test/java/com/xxxindy/ProductMicroService/ProductMicroServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xxxindy.ProductMicroService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductMicroServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
